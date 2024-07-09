229316f87110c0756b9bfc29fc01f1a261d33d13
saurabh9495 <saurabhkumar4829@gmail.com>
2024-07-09 19:37:59 +0000

    first commit

---
+++ gitopsproject/gitopsproject/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from gitopsproject.users.models import User
+from gitopsproject.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
